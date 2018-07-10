e4a2e79971473b6dcad28e361ae6f00a76423415
Fabien Udriot <fabien@omic.ch>
2018-07-10 18:07:49 +0000

    Update TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,6 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "speciality".
- *
- * Auto generated 10-01-2013 17:54
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 extension to ship common configuration specific to Ecodev',
     'description' => '',
     'author' => 'Fabien Udriot',
@@ -19,15 +9,15 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'Ecodev',
     'state' => 'stable',
     'version' => '1.0.0',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-    'suggests' => array(
-    ),
-);
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-8.99.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'suggests' => [
+    ],
+];
